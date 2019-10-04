6248c151381fc381b1ad47b8aa688678a2e38592
Jensine <jburkdesign@gmail.com>
2019-10-03 22:36:26 -0400

    - Add axios - Build out app - Add debug configs

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="fill-height">
+    <v-img :src="movie.img" />
+    <v-card-title>
+      <h2 class="caption text-truncate">{{movie.title}}</h2>
+    </v-card-title>
+    <v-card-text pa-3>
+      <v-rating small v-model="editRating"></v-rating>
+      <span class="caption">{{`${movie.rating}/5 `}}</span>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  props: {
+    movie: Object
+  },
+  data: () => ({
+    editRating: 0
+  }),
+  created() {
+    this.editRating = this.$props.movie.rating;
+  },
+  watch: {
+    editRating: function() {
+      this.$emit("change-rating", this.editRating);
+    }
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-content>
+    <v-layout justify-center row wrap fill-height>
+      <v-flex xs12 md3 pa-4>
+        <h2>My Top Rated Movies</h2>
+        <ul>
+          <li v-for="(m, index) in favoriteMovies" :key="index">{{m.title}}</li>
+        </ul>
+      </v-flex>
+      <v-flex xs12 md8 pa-2 fill-height>
+        <v-layout justify-left row wrap class="fill-height">
+          <v-flex xs12>
+            <h1>My Movies</h1>
+          </v-flex>
+          <v-flex xs6 sm3 md2 lg3 xl3 pa-2 v-for="(m, index) in movies" :key="index">
+            <MovieCard @change-rating="m.rating=$event" :movie="m"></MovieCard>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-content>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MovieCard from "../components/MovieCard";
+import axios from "axios";
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    MovieCard
+  },
+  data: () => {
+    return {
+      movies: []
+    };
+  },
+  methods: {
+    getAllMovies: async function() {
+      const url = "https://movie-api-test-page.herokuapp.com/api/movies";
+      var res = [];
+      await axios
+        .get(url)
+        .then(function(data) {
+          res = data.data;
+        })
+        .catch(function(err) {
+          // eslint-disable-next-line
+          console.log(err);
+        });
+      return res;
+    }
+  },
+  computed: {
+    favoriteMovies: function() {
+      return this.movies.filter(x => {
+        return x.rating > 4;
+      });
+    }
+  },
+  async created() {
+    this.movies = await this.getAllMovies();
   }
-}
+};
 </script>
